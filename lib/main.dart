3234ee904227710cf61d50e69e1f5c040a2ee8b1
Ian Findlay <code@findlayian.com>
2022-03-21 11:08:53 +0000

    Initialise flutter app

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+// import 'package:nc_project/pages/home_page.dart';
+// import 'package:nc_project/pages/loading_page.dart';
+// import 'package:nc_project/pages/login_page.dart';
+// import 'package:nc_project/pages/profile_page.dart';
+// import 'package:nc_project/pages/register_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Partial Pets',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: "login",
+      // routes: {
+      // "/": (context) => LoadingPage(),
+      // "register": (context) => RegisterPage(),
+      // "login": (context) => LoginPage(),
+      // "home": (context) => HomePage(),
+      // "profile": (context) => ProfilePage(),
+      // },
+    );
+  }
+}
