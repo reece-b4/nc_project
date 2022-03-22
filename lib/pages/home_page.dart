68004af16c5bee03c80825b7ab20ac2d8bd68e2b
LukeBooker <lukebooker08@gmail.com>
2022-03-22 15:58:24 +0000

    complete drop down sorting functionality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,9 +12,9 @@ class _HomePageState extends State<HomePage> {
   double? _deviceHeight, _deviceWidth;
   final List<Map> entries = <Map>[
     {
-      "pet_name": 'Rover',
+      "pet_name": "Rover",
       "age": 3,
-      "species": 'Jack Russell Terrier',
+      "species": "dogs",
       "image":
           "https://images.unsplash.com/photo-1644614398468-06fad5e8f8f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=502&q=80",
       "availabilty": true,
@@ -22,9 +22,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "Likes chicken"
     },
     {
-      "pet_name": 'Timmy',
+      "pet_name": "Timmy",
       "age": 87,
-      "species": 'Tortoise',
+      "species": "tortoises and turtles",
       "image":
           "https://images.unsplash.com/photo-1508455858334-95337ba25607?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
       "availabilty": true,
@@ -32,9 +32,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "A game of fetch takes forever"
     },
     {
-      "pet_name": 'Blobby',
+      "pet_name": "Blobby",
       "age": 7,
-      "species": 'Rat',
+      "species": "other",
       "image":
           "https://images.unsplash.com/photo-1575485670541-824ff288aaf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       "availabilty": true,
@@ -42,9 +42,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "Will bite you if provoked."
     },
     {
-      "pet_name": 'Joshua',
+      "pet_name": "Joshua",
       "age": 6,
-      "species": 'Ferret',
+      "species": "other",
       "image":
           "https://images.unsplash.com/photo-1615087240969-eeff2fa558f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
       "availabilty": true,
@@ -52,9 +52,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "Does not like being alone"
     },
     {
-      "pet_name": 'Apple',
+      "pet_name": "Apple",
       "age": 87,
-      "species": 'Hamster',
+      "species": "hamsters",
       "image":
           "https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
       "availabilty": true,
@@ -62,9 +62,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "Sensitive"
     },
     {
-      "pet_name": 'Fang',
+      "pet_name": "Fang",
       "age": 4,
-      "species": 'Boxer',
+      "species": "dogs",
       "image":
           "https://images.unsplash.com/photo-1558349699-1e1c38c05eeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
       "availabilty": true,
@@ -72,9 +72,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "High blood sugar."
     },
     {
-      "pet_name": 'Jimmy',
+      "pet_name": "Jimmy",
       "age": 8,
-      "species": 'Rabbit',
+      "species": "other",
       "image":
           "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
       "availabilty": true,
@@ -82,9 +82,9 @@ class _HomePageState extends State<HomePage> {
       "notes": "Likes eating flowers"
     },
     {
-      "pet_name": 'Oscar',
+      "pet_name": "Oscar",
       "age": 10,
-      "species": 'Guinea Pig',
+      "species": "guinea pigs",
       "image":
           "https://images.unsplash.com/photo-1533152162573-93ad94eb20f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
       "availabilty": true,
@@ -92,6 +92,10 @@ class _HomePageState extends State<HomePage> {
       "notes": "Loves a bit of fuss."
     },
   ];
+  String dropdownValue = "- Filter by -";
+
+  //STATE CHANGES
+  List filteredEntries = [];
 
   @override
   Widget build(BuildContext context) {
@@ -108,30 +112,25 @@ class _HomePageState extends State<HomePage> {
           onChanged: (value) {},
           controller: null,
           decoration: InputDecoration(
-              labelText:
-                  "Search - dogs, rabbits, budgies...",
               hintText: "Search",
+              hintStyle: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 18,
+                fontStyle: FontStyle.normal,
+              ),
               prefixIcon: Icon(Icons.search),
               border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(
                       Radius.circular(50.0)))),
         ),
       ),
-      Stack(
+      Column(
         children: <Widget>[
-          TextButton(
-            style: ButtonStyle(
-              foregroundColor:
-                  MaterialStateProperty.all<
-                      Color>(Colors.blue),
-            ),
-            onPressed: () {},
-            child: Text('TextButton'),
-          ),
+          Container(child: filterDropDown()),
           Container(
             color: Color.fromARGB(
-                255, 250, 218, 225),
-            height: _deviceHeight! * 0.91,
+                255, 214, 201, 250),
+            height: _deviceHeight! * 0.85,
             width: _deviceWidth!,
             child: petCards(),
           )
@@ -140,9 +139,72 @@ class _HomePageState extends State<HomePage> {
     ])));
   }
 
+  Widget filterDropDown() {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(
+          color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+          filteredEntries = entries
+              .where((pet) => dropdownValue
+                  .contains(pet["species"]))
+              .toList();
+          if (filteredEntries.length == 0 &&
+              dropdownValue != "- Filter by -") {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: new Text("Sorry!"),
+                  content: new Text(
+                      "There are currently no ${dropdownValue}"),
+                  actions: <Widget>[
+                    new ElevatedButton(
+                      child: new Text("OK"),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+          ;
+        });
+      },
+      items: <String>[
+        "- Filter by -",
+        "dogs",
+        "birds",
+        "hamsters",
+        "guinea pigs",
+        "tortoises and turtles",
+        "other"
+      ].map<DropdownMenuItem<String>>(
+          (String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+
   Widget petCards() {
     return ListView.separated(
-      itemCount: entries.length,
+      itemCount: (filteredEntries.length == 0
+          ? entries.length
+          : filteredEntries.length),
       separatorBuilder:
           (BuildContext context, int index) =>
               const Divider(),
@@ -156,8 +218,11 @@ class _HomePageState extends State<HomePage> {
             borderRadius:
                 BorderRadius.circular(40),
             image: DecorationImage(
-              image: NetworkImage(
-                  "${entries[index]['image']}"),
+              image: NetworkImage((filteredEntries
+                          .length ==
+                      0
+                  ? "${entries[index]["image"]}"
+                  : "${filteredEntries[index]["image"]}")),
               fit: BoxFit.cover,
             ),
           ),
@@ -198,10 +263,14 @@ class _HomePageState extends State<HomePage> {
                                       .min,
                               children: <Widget>[
                                 Text(
-                                  "${entries[index]['pet_name']}, ${entries[index]['age']}",
+                                  (filteredEntries
+                                              .length ==
+                                          0
+                                      ? "${entries[index]["pet_name"]}, ${entries[index]["age"]}"
+                                      : "${filteredEntries[index]["pet_name"]}, ${filteredEntries[index]["age"]}"),
                                   style: TextStyle(
                                       fontFamily:
-                                          'Roboto',
+                                          "Roboto",
                                       decoration:
                                           TextDecoration
                                               .none,
@@ -211,10 +280,10 @@ class _HomePageState extends State<HomePage> {
                                           .white),
                                 ),
                                 Text(
-                                  "${entries[index]['distance']}",
+                                  "${entries[index]["distance"]}",
                                   style: TextStyle(
                                       fontFamily:
-                                          'Roboto',
+                                          "Roboto",
                                       decoration:
                                           TextDecoration
                                               .none,
@@ -236,7 +305,7 @@ class _HomePageState extends State<HomePage> {
                                     Colors.pink,
                                 size: 40.0,
                                 semanticLabel:
-                                    'Heart',
+                                    "Heart",
                               ),
                             )
                           ]))
