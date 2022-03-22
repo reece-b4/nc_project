c89d89d6649bddaf0b4d5c17d07358d6018452b3
nathanielpells <93220158+nathanielpells@users.noreply.github.com>
2022-03-22 16:11:09 +0000

    styled the titles for my pets and my reviews plas added icons to each.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -33,7 +33,6 @@ class _ProfilePageState extends State<ProfilePage> {
                   _titleWidget(),
                   _UserCard(),
                   _myPetsTitle(),
-                  _addOrDeleteButtons(),
                   _userPetsCard(),
                   _myReviewsTitle(),
                   _userReviewsCard(),
@@ -149,40 +148,74 @@ class _ProfilePageState extends State<ProfilePage> {
             const Text("Dog",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
             const Text("Dalmatian",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600))
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+            Icon(
+              Icons.delete,
+              color: Colors.red,
+            ), // <-- Icon
           ],
         ));
   }
 
   Widget _myPetsTitle() {
-    return const Text("My Pets",
-        style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600));
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+        bottom: 20.0,
+      ),
+      child: Row(
+        children: [
+          const Text("My Pets",
+              style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600)),
+          Padding(padding: EdgeInsets.all(5)),
+          _addButton()
+        ],
+      ),
+    );
   }
 
-  Widget _addOrDeleteButtons() {
-    return Row(
-      children: [
-        MaterialButton(
-          onPressed: () {},
-          minWidth: _deviceWidth! * 0.10,
-          height: _deviceHeight! * 0.02,
-          color: Colors.green,
-          child: const Text(
-            "Add Pet",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 10,
-              fontWeight: FontWeight.w600,
+  Widget _addButton() {
+    return SizedBox.fromSize(
+      size: Size(45, 45),
+      child: ClipOval(
+        child: Material(
+          color: Colors.purple,
+          child: InkWell(
+            splashColor: Colors.green,
+            onTap: () {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.pets,
+                  color: Colors.white,
+                ), // <-- Icon
+                Text("Add", style: TextStyle(color: Colors.white)), // <-- Text
+              ],
             ),
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 
   Widget _myReviewsTitle() {
-    return const Text("My Reviews",
-        style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600));
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+        bottom: 20.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text("My Reviews",
+              style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600)),
+          Icon(Icons.reviews),
+        ],
+      ),
+    );
   }
 
   Widget _userReviewsCard() {
