bad4af919fe699601154b074fe16ff9eb9bb803a
Ian Findlay <code@findlayian.com>
2022-03-26 17:13:07 +0000

    Change initialRoute to login

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      initialRoute: "chat",
+      initialRoute: "login",
       routes: {
         // "/": (context) => LoadingPage(),
         "chat": (context) => const MyHomePage(),
