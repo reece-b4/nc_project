e76e3a0e4e095195b67ff5098ce554c267304311
Ian Findlay <code@findlayian.com>
2022-03-27 21:07:45 +0100

    Complete hardcoded conversation list

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:nc_project/pages/widgets/conversations_list.dart';
+
+import 'models/chat_users_model.dart';
 
 class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
   @override
   _ChatPageState createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatUsers> chatUsers = [
+    ChatUsers(
+        name: "Jane Russel",
+        messageText: "Awesome Setup",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "Now"),
+    ChatUsers(
+        name: "Glady's Murphy",
+        messageText: "That's Great",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "Yesterday"),
+    ChatUsers(
+        name: "Jorge Henry",
+        messageText: "Hey where are you?",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "31 Mar"),
+    ChatUsers(
+        name: "Philip Fox",
+        messageText: "Busy! Call me in 20 mins",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "28 Mar"),
+    ChatUsers(
+        name: "Debra Hawkins",
+        messageText: "Thankyou, It's awesome",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "23 Mar"),
+    ChatUsers(
+        name: "Jacob Pena",
+        messageText: "will update you in evening",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "17 Mar"),
+    ChatUsers(
+        name: "Andrey Jones",
+        messageText: "Can you please share the file?",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "24 Feb"),
+    ChatUsers(
+        name: "John Wick",
+        messageText: "How are you?",
+        imageURL: "https://i.pravatar.cc/300",
+        time: "18 Feb"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +82,21 @@ class _ChatPageState extends State<ChatPage> {
                         borderSide: BorderSide(color: Colors.grey.shade100)),
                   ),
                 ),
+              ),
+              ListView.builder(
+                itemCount: chatUsers.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(top: 16),
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return ConversationList(
+                    name: chatUsers[index].name,
+                    messageText: chatUsers[index].messageText,
+                    imageUrl: chatUsers[index].imageURL,
+                    time: chatUsers[index].time,
+                    isMessageRead: (index == 0 || index == 3) ? true : false,
+                  );
+                },
               ),
             ],
           ),
